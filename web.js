62f8aec03036c92b370644529325f98b19d4dc94
Stephen Mwanje <stephen.mwanje@gmail.com>
2013-07-14 16:14:29 +0200

    web.js first edit

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer("Hello World 2!", "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
